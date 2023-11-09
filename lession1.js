278b0c0ed6bd61132186c293db256b5040a729bb
lethitan <lethitan2412001@gmail.com>
2023-11-09 23:40:51 +0700

    lession 1

---
+++ lession1.js
@@ -0,0 +1,91 @@
+// console.log("Hello JavaScript");
+// {
+//     console.log("Hello JavaScript");
+//     {
+//         console.log("Hello JavaScript");
+//     }
+// }
+
+// {
+ //   let a =3;
+//     {
+//         console.log(a);
+//     }
+// }
+//console.log(a);  // không thể sử dụng biến cấp thấp hơn ở trong block cao cấp hơn
+
+  //  let a =2 // không thể khởi tạo giá trị nhiều lần bằng biến let
+
+// {
+//     var a =7
+// }
+// console.log(a); // biến var không phân biệt cấp bậc của các block, có thể call đc ở mọi cấp
+
+// var a = 7;
+// var a = 23;
+// console.log(a); //=> a = 23. có thể khơi tạo giá trị nhiều lần
+
+// const Pi = 3.14;
+
+// Pi =3;
+
+// const objA = {};
+// console.log(objA);
+// objA["name"] = "Choco";
+// console.log(objA);
+
+// let  a= 4;
+// let b=8;
+// let c = a +b;
+// console.log(c);
+
+// let  a= 4;
+// let b=8;
+// let c = b%a;
+// console.log(c);
+
+// let a =1 ;
+// console.log(a++); - in truoc cong sau
+// console.log(a);
+
+// let a =1 ;
+// console.log(++a); - cong truoc in sau
+// console.log(a);
+
+//let a =1 ;
+// console.log(a--);
+// console.log(a);
+
+//let a =1 ;
+// let b = 5;
+// a **=b;
+// console.log(a);
+
+
+// let a =6;
+// let b=6;
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a==b);
+// console.log(a=b); // it's not a function
+
+// if(a=b){ // statement alway true
+//     let a = "Choco";
+//     let b = "Le";
+//     console.log(a+b);
+// }
+
+
+// console.log("5" + 6);
+// console.log("5" == 5);
+// console.log("5" === 5);
+
+
+let a =5;
+ console.log(a > 2 && a <6);
+ console.log(a > 2 || a <6);
+ console.log(a%2 == 0 && (a > 2 || a <6));
+ console.log(!(a==6));
+
+
+
