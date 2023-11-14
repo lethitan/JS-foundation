6374f3747b54e075ea70905ae9f1af79abe7d847
lethitan <lethitan2412001@gmail.com>
2023-11-14 23:05:19 +0700

    Lession 2 - Primitive DataType

---
+++ lession2.js
@@ -0,0 +1,74 @@
+// console.log("Lession 2");
+// let isMale = false;
+// let a = isMale ? 18 : 20;
+// console.log(a);
+
+// a = undefined;
+// b = a??111;
+
+// console.log(b)
+
+
+// let a = false;
+//console.log(typeof a);
+
+// let a = 0.1;
+// let b = 0.2;
+// console.log(a+b);
+
+// console.log("20"*"2"); // 40
+// let b= 10;
+// console.log(b/"cc"); // NaN
+// console.log(b/0);
+
+// let a = new Number(10);
+// let b = 10;
+
+// console.log(a==b);
+
+// let a = new Number(10);
+// let b = new Number(10);
+// console.log(a.valueOf() == b.valueOf());
+// console.log(a==b);
+
+// console.log(Number.isNaN(NaN));
+// console.log(Number.isNaN("NaN"));
+// console.log(Number.MAX_VALUE);
+
+// console.log((2.555).toFixed(1));
+
+// console.log("i'm \"Choco\"")
+// console.log("mmm" == "l");
+// // console.log("abcd".substring(1,3));
+// console.log("abcd".toLowerCase());
+// console.log("abcd".toUpperCase());
+
+// console.log("abcd".replace("b","ttt"));
+
+// console.log("12345".replace(/1/,"T"));
+// console.log("12345".replacAll(/1/,"T"));
+// console.log("12345".replace(/1/g,"T"));
+
+// let student = {
+//     "name":"Tan",
+//     "age:12,
+//     "sex":"male"
+// }
+
+let student = {
+    name:"Tan",
+    age: 12,
+    sex:"male",
+    getNameAndAddress: function(){
+        return (this.name + " " + this. address);
+    }
+}
+console.log(student);
+student.address = "123";
+console.log(student);
+
+console.log(student.age);
+console.log(student["sex"]);
+console.log(Object.keys(student));
+
+console.log(student.getNameAndAddress())
