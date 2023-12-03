3612c754cf84d96bcd07a4e91ae0d2d75d111bc6
lethitan <lethitan2412001@gmail.com>
2023-12-03 19:12:29 +0700

    Lession4 : switch-case

--- lession4/excercise1.js
+++ lession4/excercise1.js
@@ -1,54 +1,73 @@
-function acreage(type, a) {
-    if (a instanceof Array) {
+const TYPE = {
+    REC : "REACTANGLE",
+    TRI : "TRIANGLE",
+    CY : "CYCLE",
+}
+
+function acreageRetangle(length, width){
+    if (typeof length != "number" || typeof width != "number") {
+        throw "input data is invalid";
+    }
+    if (length == 0 || width == 0) {
+        throw "Edge can't equal 0";
+    }
+    return length*width;
+}
+
+function acreageTriangle(a,b,c){
+    if (typeof a != "number" || typeof b != "number" || typeof c != "number") {
+        throw "input data is invalid";
+    }
+    if (a == 0 || b == 0 || c == 0) {
+        throw "Edge can't equal 0";
+    }
+    let p = (a + b + c) / 2
+    let h = 2 * (Math.sqrt(p * (p - a) * (p - b) * (p - c))) / a;
+    return 0.5 * a * h;
+}
+
+function acreageCycle(r){
+    if (typeof r != "number") {
+        throw "input data is invalid";
+    }
+    if (r == 0) {
+        throw "R can't equal 0";
+    }
+    const pi = 3.14;
+    return pi * r * r;
+}
+
+function acreage(type, params) {
+    let rs = 0;
+    if(params instanceof Array){
         switch (type) {
-            case "REC":
-                if (a.length != 2) {
+            case TYPE.REC:
+                if (params.length != 2) {
                     throw "it isn't a retangle";
                 }
-                if (typeof a[0] != "number" || typeof a[1] != "number") {
-                    throw "input data is invalid";
-                }
-                if (a[0] == 0 || a[1] == 0) {
-                    throw "Edge can't equal 0";
-                }
-                let acrREC = a[0] * a[1];
-                console.log("acreage of rectangle: " + acrREC);
+                rs = acreageRetangle(params[0],params[1]);
                 break;
-            case "TRI":
-                if (a.length != 3) {
-                    throw "it isn't a triangle  or input data is invalid ";
-                } if (typeof a[0] != "number" || typeof a[1] != "number" || typeof a[2] != "number") {
-                    throw "input data is invalid";
-                }
-                if (a[0] == 0 || a[1] == 0 || a[2] == 0) {
-                    throw "Edge can't equal 0";
-                }
-                let p = (a[0] + a[1] + a[2]) / 2
-                let h = 2 * (Math.sqrt(p * (p - a[0]) * (p - a[1]) * (p - a[2]))) / a[0];
-                let acrTRI = 0.5 * a[0] * h;
-                console.log("acreage of triangle: " + acrTRI);
+            case TYPE.TRI:
+                if (params.length != 3) {
+                    throw "it isn't a triangle";
+                } 
+                rs = acreageTriangle(params[0],params[1],params[2]);
                 break;
-            case "CY":
-                if (a.length != 1) {
+            case TYPE.CY:
+                if (params.length != 1) {
                     throw "it isn't a cycle";
-                } if (typeof a[0] != "number") {
-                    throw "input data is invalid";
-                }
-                if (a[0] == 0) {
-                    throw "R can't equal 0";
-                }
-                const pi = 3.14;
-                let acrCY = pi * a[0] * a[0];
-                console.log("acreage of cycle: " + acrCY);
+                } 
+                rs = acreageCycle(params[0]);
                 break;
             default:
                 throw "Not a rectangle or triangle or cycle";
         }
     } else
-        throw "Please input a is an array";
+        throw "Please input edges in an array";
+    return rs;
 }
 try {
-    acreage("REC", [6, 0]);
+    console.log(acreage(TYPE.REC, [6, 7]));
     // acreage("TRI",[1,3,3]);
     // acreage("CY",[3]);
 } catch (error) {
